439599e89ee2cf1f06f84f78a3243437b9ab4143
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2013-07-08 22:27:40 +0200

    Woops wrong filename

--- src/com/android/python27/config/GlobalConstants.java
+++ src/com/android/python27/config/GlobalConstants.java
@@ -25,7 +25,7 @@ package com.android.python27.config;
 public class GlobalConstants {
 
     public static final String PYTHON_MAIN_SCRIPT_NAME = "hello.py";
-    public static final String PYTHON_PROJECT_ZIP_NAME = "python-keyring-lib.zip";
+    public static final String PYTHON_PROJECT_ZIP_NAME = "python_keyring_lib.zip";
     public static final String PYTHON_ZIP_NAME = "python_27.zip";
     public static final String PYTHON_EXTRAS_ZIP_NAME = "python_extras_27.zip";
 
